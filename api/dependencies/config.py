49425f3cd79fab937338a0ddc3dabf41cdfae219
HaidaMarese <hmakouan06@gmail.com>
2025-01-08 10:16:42 -0500

    Initial commit with FastAPI CRUD application and database functionality

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,8 @@
 class conf:
     db_host = "localhost"
-    db_name = "sandwich_maker_api"
+    db_name = "sandwich_maker_api_team"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    db_password = "Likibi@87"
     app_host = "localhost"
     app_port = 8000
